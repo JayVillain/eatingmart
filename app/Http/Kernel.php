e47060d9f06cddc73e74d7df809df9fa32f29f27
JayVillain <saipi3825@gmail.com>
2025-08-25 18:17:54 +0000

    Aug 26, 2025, 1:17 AM

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // Baris ini adalah yang perlu Anda tambahkan
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
